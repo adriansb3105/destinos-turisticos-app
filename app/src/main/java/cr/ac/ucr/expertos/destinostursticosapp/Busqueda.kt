fa07bc4019cf4bb0c8ba38f3dfd39bd08f0ef6fd
Adrián Serrano <adriansb3105@gmail.com>
2020-06-11 16:31:03 -0600

    Se agregan las llamadas a la API REST

--- app/src/main/java/cr/ac/ucr/expertos/destinostursticosapp/Busqueda.kt
+++ app/src/main/java/cr/ac/ucr/expertos/destinostursticosapp/Busqueda.kt
@@ -2,6 +2,8 @@ package cr.ac.ucr.expertos.destinostursticosapp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_busqueda.*
 
@@ -10,7 +12,92 @@ import kotlinx.android.synthetic.main.fragment_busqueda.*
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         buttonShowDialog.setOnClickListener { showDialog() }
+
+        spDestino()
+        spPersonas()
+        spEdades()
+        spIntereses()
     }
+
+        private fun spDestino() {
+            val adapter = context?.let { ArrayAdapter(it, android.R.layout.simple_spinner_item, listOf("Playa", "Ciudad", "Montaña", "Rural")) }
+            if (adapter != null) {
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            }
+            spDestino.adapter = adapter
+
+            spDestino.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+
+                override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                    // either one will work as well
+                    // val item = parent.getItemAtPosition(position) as String
+                    val item = adapter?.getItem(position)
+                }
+            }
+        }
+
+        private fun spPersonas() {
+            val adapter = context?.let { ArrayAdapter(it, android.R.layout.simple_spinner_item, listOf("Familia", "Pareja", "Amigos", "Solo")) }
+            if (adapter != null) {
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            }
+            spPersonas.adapter = adapter
+
+            spPersonas.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+
+                override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                    // either one will work as well
+                    // val item = parent.getItemAtPosition(position) as String
+                    val item = adapter?.getItem(position)
+                }
+            }
+        }
+
+        private fun spEdades() {
+            val adapter = context?.let { ArrayAdapter(it, android.R.layout.simple_spinner_item, listOf("De 18 a 35 años", "De 36 a 55 años", "Mayores o iguales a 56 años")) }
+            if (adapter != null) {
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            }
+            spEdades.adapter = adapter
+
+            spEdades.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+
+                override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                    // either one will work as well
+                    // val item = parent.getItemAtPosition(position) as String
+                    val item = adapter?.getItem(position)
+                }
+            }
+        }
+
+        private fun spIntereses() {
+            val adapter = context?.let { ArrayAdapter(it, android.R.layout.simple_spinner_item, listOf("Cultura", "Historia", "Relajación", "Actualidad", "Gastronomía", "Entretenimiento")) }
+            if (adapter != null) {
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            }
+            spIntereses.adapter = adapter
+
+            spIntereses.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+
+                override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                    // either one will work as well
+                    // val item = parent.getItemAtPosition(position) as String
+                    val item = adapter?.getItem(position)
+                }
+            }
+        }
 
         private fun showDialog() {
             val dialogFragment = Resultados(this)
--- app/src/main/java/cr/ac/ucr/expertos/destinostursticosapp/Resultados.kt
+++ app/src/main/java/cr/ac/ucr/expertos/destinostursticosapp/Resultados.kt
@@ -1,11 +1,19 @@
 package cr.ac.ucr.expertos.destinostursticosapp
 
+import android.app.Activity
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import androidx.fragment.app.DialogFragment
 import kotlinx.android.synthetic.main.activity_resultados.*
+import org.json.JSONArray
+import java.net.HttpURLConnection
+import java.net.URL
+
 class Resultados(private val callbackListener: CallbackListener) : DialogFragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,5 +35,107 @@ class Resultados(private val callbackListener: CallbackListener) : DialogFragmen
             dismiss()
         }
 
+        //var listView = findViewById<ListView>(R.id.listview);
+
+
+
+
+        val url = "https://mysafeinfo.com/api/data?list=presidents&format=json"
+
+        AsyncTaskHandleJson().execute(url)
+    }
+
+    inner class AsyncTaskHandleJson : AsyncTask<String, String, String>() {
+        override fun doInBackground(vararg url: String?): String {
+            var text: String = ""
+            val connection = URL(url[0]).openConnection() as HttpURLConnection
+
+            try {
+                connection.connect()
+                text = connection.inputStream.use { it.reader().use{reader -> reader.readText() } }
+            } finally {
+                connection.disconnect()
+            }
+
+            return text
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            handleJson(result)
+        }
+    }
+
+    private fun handleJson(jsonString: String?) {
+        val jsonArray = JSONArray(jsonString)
+        val list = ArrayList<President>()
+        var x = 0
+
+        while (x < jsonArray.length()) {
+            val jsonObject = jsonArray.getJSONObject(x)
+
+            list.add(President(
+                jsonObject.getInt("ID"),
+                jsonObject.getString("FullName"),
+                jsonObject.getString("Party"),
+                jsonObject.getString("Terms")
+            )
+            )
+
+            x++
+        }
+
+        //val adapter = getActivity()?.getApplicationContext()?.let { ListAdapte(it, list) }
+        //presidents_list.adapter = adapter
+
+
+
+        val customAdptor = CustomAdptor(this, list)
+        listview.adapter=customAdptor
+
+        listview.setOnItemClickListener{ parent, view, position, id ->
+            Toast.makeText(getActivity()?.getApplicationContext(), "You Clicked:"+" "+list.get(position), Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+class CustomAdptor(private val context: DialogFragment, private val lista: ArrayList<President>): BaseAdapter() {
+    //Array of fruits names
+    //var names = arrayOf("Apple", "Strawberry", "Pomegranates", "Oranges", "Watermelon", "Apple", "Strawberry", "Pomegranates", "Oranges", "Watermelon")
+    //Array of fruits desc
+    //var desc = arrayOf("Malus Domestica", "Fragaria Ananassa ", "Punica Granatum", "Citrus Sinensis", "Citrullus Vulgaris", "Musa Acuminata", "Actinidia Deliciosa", "Solanum Lycopersicum", "Vitis vinifera", "Citrullus Vulgaris")
+
+    //Array of fruits images
+    var image = intArrayOf(R.drawable.apple, R.drawable.strawberry, R.drawable.pomegranates, R.drawable.oranges, R.drawable.watermelon)
+
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        val inflater = context.layoutInflater
+        val view1 = inflater.inflate(R.layout.row_data,null)
+        val fimage = view1.findViewById<ImageView>(R.id.fimageView)
+        var fName = view1.findViewById<TextView>(R.id.fName)
+        var fDesc = view1.findViewById<TextView>(R.id.fDesc)
+        fimage.setImageResource(image[p0])
+
+
+        fName.setText(lista.get(p0).name)
+        fDesc.setText(lista.get(p0).politic)
+
+
+
+        //Log.d(p0.toString(), lista.get(p0).name)
+        return view1
+    }
+
+    override fun getItem(p0: Int): Any {
+        return image[p0]
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return p0.toLong()
+    }
+
+    override fun getCount(): Int {
+        return image.size
     }
 }
